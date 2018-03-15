fc0a14c21a17eb79d61b483d21af5bdadaf9dd53
ibryans <bryans0701@gmail.com>
2018-03-15 16:03:52 -0300

    Splash Screen Feito

---
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package br.ufmg.coltec.tp.e03_layout;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+
+}
