7a04f6f0a1346567e4bfed396dc1b79cb1cc7be5
ibryans <bryans0701@gmail.com>
2018-03-23 15:57:50 -0300

    Quase finalizado

--- app/src/main/java/br/ufmg/coltec/tp/e03_layout/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/MainActivity.java
@@ -3,6 +3,8 @@ package br.ufmg.coltec.tp.e03_layout;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -11,8 +13,48 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this,
-                                    PrevisaoActivity.class);
-        startActivity(intent);
+        Button splash = findViewById(R.id.splash_button);
+        Button noticia = findViewById(R.id.noticia_button);
+        Button previsao = findViewById(R.id.previsao_button);
+        Button formulario = findViewById(R.id.formulario_button);
+
+        splash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        SplashScreenActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        noticia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        NoticiaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        previsao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        PrevisaoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        formulario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        FormularioActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
