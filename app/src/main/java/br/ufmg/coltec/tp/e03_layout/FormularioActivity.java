ce7c9287ee2e3e77319b600a3e1d0f271db03620
ibryans <bryans0701@gmail.com>
2018-04-05 11:10:32 -0300

    Dialog finalizado - Falta internacionalização

--- app/src/main/java/br/ufmg/coltec/tp/e03_layout/FormularioActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/FormularioActivity.java
@@ -1,6 +1,8 @@
 package br.ufmg.coltec.tp.e03_layout;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,5 +14,42 @@ public class FormularioActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        Button btn = findViewById(R.id.btn_finalizar);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(FormularioActivity.this);
+
+
+                // Define Parâmetros para o Dialog
+                alertBuilder.setIcon(R.drawable.ic_launcher_foreground);
+                alertBuilder.setTitle("Confirmação");
+                alertBuilder.setMessage("Confirmar envio de cadastro?");
+
+
+                alertBuilder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getBaseContext(), "Cadastro realizado!", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+
+                alertBuilder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getBaseContext(), "Cadastro não realizado", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+
+                AlertDialog dialog = alertBuilder.create();
+                dialog.show();
+            }
+        });
     }
 }
